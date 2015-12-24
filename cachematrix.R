9980e5fbe9384ec7a5d74a59144ef47e6ec5a30a
z93 <temri001@umn.edu>
2015-12-24 13:15:22 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<- NULL
+  function (y){
+    x<<-y
+    inv<<-NULL 
+  }
+get<- function() x
+setinv<- function(inverse) 
+  getinv<- function()inv
+list(set=set,
+     get=get,
+     setinv=setinv,
+     getinv=getinv)
 }
+cachesolve<-function(x, ...){
+  inv<-x$getinv()
+  if(!is.null(inv)) {
+    message("retrieving cached data")
+    return(inv)
+  }
+  mat<-x$get()
+  inv<-solve(mat,...)
+  x$setinv(inv)
+  inv
+}
+
 
 
 ## Write a short comment describing this function
